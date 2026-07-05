14e83c7bc7978d181e40a3d841bbb67197629a49
Nciibi <ncibia77@gmail.com>
2026-07-05 16:12:22 +0100

    v0.1.57

--- tools/fixtures/.chronam/tb_counter.vhd
+++ tools/fixtures/.chronam/tb_counter.vhd
@@ -1,6 +1,6 @@
 -- Auto-generated testbench by Chronam
 -- Entity: counter
--- Generated: 2026-07-05T15:07:35.899Z
+-- Generated: 2026-07-05T15:12:17.121Z
 
 library ieee;
 use ieee.std_logic_1164.all;
